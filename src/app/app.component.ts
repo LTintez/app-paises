35b58db3fd776da328d07fc0ffc700339d0dbddf
MaxiLedesma04 <maxinglede@gmail.com>
2024-08-29 10:39:30 -0300

    buscador mas diebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 import { TablaPaisesComponent } from './components/tabla-paises/tabla-paises.component';
 import { RegionPageComponent } from './pages/region-page/region-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RegionPageComponent
-  ],
+  imports: [RouterOutlet, RegionPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-paises';
