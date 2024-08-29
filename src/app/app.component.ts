8cdf91f4ec93d973f25c068d64aecd62518c614f
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-29 09:46:46 -0300

    adding table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablaPaisesComponent } from './components/tabla-paises/tabla-paises.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TablaPaisesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
