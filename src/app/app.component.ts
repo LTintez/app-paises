3aaed260bb704f16c66394929920b497d606d1b3
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-29 11:13:48 -0300

    configurando rutas, sidebar y paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { TablaPaisesComponent } from './components/tabla-paises/tabla-paises.component';
 import { RegionPageComponent } from './pages/region-page/region-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegionPageComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaisPageComponent } from './pages/pais-page/pais-page.component';
+import { RegionPageComponent } from './pages/region-page/region-page.component';
+import { CapitalPageComponent } from './pages/capital-page/capital-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PaisPageComponent },
+    { path: 'region', component: RegionPageComponent },
+    { path: 'capital', component: CapitalPageComponent },
+];
